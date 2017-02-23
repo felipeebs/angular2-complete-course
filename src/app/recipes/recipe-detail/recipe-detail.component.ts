9e0fd1e1696b229d2798b14593cfc52553b56cbd
Felipe Schmitz <felipeebs@gmail.com>
2017-02-23 08:10:32 -0300

    Recipe detail databinding

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from "../recipe";
+
+@Component({
+  selector: 'rb-recipe-detail',
+  templateUrl: './recipe-detail.component.html'
+})
+export class RecipeDetailComponent {
+  @Input() selectedRecipe: Recipe
+
+}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe} from "../recipe";
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from "../recipe";
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
   recipes: Recipe[] = []
+  @Output() recipeSelected = new EventEmitter<Recipe>()
   recipe = new Recipe('Dummy', 'Dummy description', 'https://static.thenib.com/usq/1d97429f-4a64-4d52-bfdb-c36172c05228/this-is-not-fine-001-dae9d5.png')
 
-  constructor() { }
-
-  ngOnInit() {
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe)
   }
-
 }
