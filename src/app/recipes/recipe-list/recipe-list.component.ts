dc5a7e0e697b594c1d07cdd1b25e73bd48d300f5
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-03-01 19:22:26 -0300

    Ingredient list with *ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,11 @@ import { Recipe } from "../recipe";
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = []
+  recipes: Recipe[] = [
+    new Recipe('Dummy1', 'Dummy1 description', 'https://static.thenib.com/usq/1d97429f-4a64-4d52-bfdb-c36172c05228/this-is-not-fine-001-dae9d5.png', []),
+    new Recipe('Dummy2', 'Dummy2 description', 'https://static.thenib.com/usq/1d97429f-4a64-4d52-bfdb-c36172c05228/this-is-not-fine-001-dae9d5.png', [])
+  ]
   @Output() recipeSelected = new EventEmitter<Recipe>()
-  recipe = new Recipe('Dummy', 'Dummy description', 'https://static.thenib.com/usq/1d97429f-4a64-4d52-bfdb-c36172c05228/this-is-not-fine-001-dae9d5.png')
 
   onSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe)
